9dcee8573f91f3d5ccb462148d311d66faa88af2
Shashwat Dalal <spd16@ic.ac.uk>
2019-02-19 17:01:19 +0000

    implemented scout.ex

---
+++ lib/scout.ex
@@ -0,0 +1,39 @@
+# Ilyas Saltykov (is916) and Shashwat Dalal (spd16)
+
+# distributed algorithms, n.dulay 11 feb 19
+# coursework 2, paxos made moderately complex
+
+defmodule Scout do
+  def start(l_pid, acceptors, b_num) do
+    # send intial of b_num to all acceptors
+    for a <- acceptors, do: send a, {:scout_p1a, self(), b_num}
+    next l_pid, acceptors, b_num, MapSet.new(acceptors), MapSet.new()
+  end
+
+  defp next(l_pid, acceptors, b_num, waitfor, p_vals) do
+    receive do
+      {:acceptor_p1b, a_pid, a_b_num, a_p_vals} ->
+        if b_num == a_b_num do
+          # update our proposed values
+          p_vals = MapSet.union(p_vals, a_p_vals)
+          waitfor = MapSet.delete(waitfor, a_pid)
+          if MapSet.size(waitfor) < (MapSet.size(acceptors) / 2) do
+            # majority of acceptors have made a promise on our proposal
+            send l_pid, {:scout_adopted, b_num, p_vals}
+            # kill node
+            exit()
+          end
+        else
+          # an acceptor has rejected our proposal because it
+          # has already received a higher ballot number
+          send l_pid, {:scout_preempted, a_b_num}
+          exit()
+        end
+    end
+  end
+
+  defp exit() do
+    Process.exit(self(),:normal)
+  end
+
+end
